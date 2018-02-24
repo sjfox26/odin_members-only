1c911e714d4620de80d993997c3e53ffe940b40d
Scott Fox <scottfox87@gmail.com>
2018-02-24 13:21:03 -0600

    add current_user and sign_out methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,21 @@ class ApplicationController < ActionController::Base
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        sign_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def sign_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
