ff384f654630f6f34c09ddaf0b7c51bf61f56f2d
Scott Fox <scottfox87@gmail.com>
2018-02-24 12:39:39 -0600

    add ability to remember users with token and corresponding digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :remember
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -7,4 +9,18 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token.to_s))
+    #self.remember_digest = User.digest(remember_token.to_s)
+  end
 end
